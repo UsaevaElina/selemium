75420cf2fa50a6f0aee88a025103a04024130f84
ElinaUsaeva <Usaeva1@yandex.ru>
2023-05-21 12:04:17 +0300

    Positive Test

--- src/test/java/OrderTests.java
+++ src/test/java/OrderTests.java
@@ -1,12 +1,16 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class OrderTests {
     private WebDriver driver;
+    @BeforeAll
+    static void setUpAll() {
+// убедитесь, что файл chromedriver.exe расположен именно в каталоге C:\tmp
+        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+    }
 
     @BeforeEach
     void setUp() {
@@ -22,7 +26,15 @@ public class OrderTests {
         driver = null;
     }
     @Test
-    void shouldTestSomething() {
-        throw new UnsupportedOperationException();
+    void shouldTestPositivResult() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Усаева Элина");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79996705891");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button__text")).click();
+        String expected = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String acrual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        Assertions.assertEquals(expected,acrual);
+
     }
 }
